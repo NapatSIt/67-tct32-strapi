9afa45813d8b92a9aac4d99a04f62ac355305b24
Napat_SIt <napats.be1@gmail.com>
2024-02-27 16:13:28 +0700

    add 27 feb

--- src/api/student/content-types/student/lifecycles.js
+++ src/api/student/content-types/student/lifecycles.js
@@ -1,6 +1,11 @@
+const md5 = require("md5");
 module.exports = {
-  async beforeCreate() {
+  async beforeCreate(event) {
     // ...
-    console.log("beforeCreate is worked...");
+    console.log("beforeCreate is worked...", event.params.data);
+    //event.params.data.mobile = "KFC1150";
+    event.params.data.mobile = md5(event.params.data.mobile);
+
+    return;
   },
 };
